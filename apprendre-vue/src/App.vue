399ddbba6403ac63ae4c711b08fb671994bf91a5
Guiboy974 <139962386+Guiboy974@users.noreply.github.com>
2024-12-18 10:19:35 +0100

    modif, v-if by function toSorted

--- apprendre-vue/src/App.vue
+++ apprendre-vue/src/App.vue
@@ -8,55 +8,43 @@
 li:hover {
   cursor: pointer;
 }
+
+ul.list-group.bg-transparent {
+  background-color: transparent !important;
+}
 </style>
 
 <template>
   <main class="container">
-    <h2 class="mt-2">TODO LIST</h2>
-
+    <h2 class="mt-2 text-center">TODO LIST</h2>
 
     <form action="" @submit.prevent="addTask" class="mt-3">
-      <div class="mb-3">
-        <label for="inputTask" class="form-label">Ajouter une tâche</label>
-        <input type="text" class="form-control" id="inputTask" aria-describedby="inputTask" v-model="task">
+      <div class="mb-3 input-group">
+        <input type="text" class="form-control" id="inputTask" aria-describedby="inputTask" v-model="task"
+          placeholder="nouvelle tâche">
+        <button class="btn btn-outline-warning" :disabled="task.length === 0">Ajouter</button>
       </div>
-      <button class="btn btn-primary" :disabled="task.length === 0">Ajouter</button>
     </form>
-    <section v-if="todos.length === 0">
-      <h3>pas de tache ne cours</h3>
+    <section class="mt-3 text-center" v-if="todos.length === 0">
+      <h3>pas de tâches en cours</h3>
     </section>
-
-    <section class="mt-3" v-else="todos.length !== 0">
-      <h3>Mes tâches</h3>
-      <select class="form-select" aria-label="select todolist" @change="sortedTodo">
-        <option selected value="1">toutes les tâches</option>
-        <option value="2">à faire</option>
-        <option value="3">terminer</option>
-      </select>
-
-      <ul class="list-group" v-for="todo in todos">
-        <li class="list-group-item d-flex align-items-center" v-if="todo.completed===false" :key="todo.id">
+    <section class="mt-3">
+      <ul class="list-group bg-transparent">
+        <li class="list-group-item d-flex align-items-center" v-for="todo in sortedTodo()" :key="todo.id">
           <div class="form-check">
             <label :class="{ terminer: todo.completed }">
-              <input type="checkbox" v-model="todo.completed" @click="replaceTodo">
+              <input class="form-check-input" type="checkbox" v-model="todo.completed" @click="replaceTodo">
               {{ todo.name }}
             </label>
           </div>
-          <button @click="deleteTask" class="btn btn-outline-primary ms-auto">supprimer</button>
+          <button @click="deleteTask" class="btn btn-warning ms-auto">supprimer</button>
         </li>
       </ul>
-      <ul class="list-group" v-for="todo in todos">
-        <li class="list-group-item d-flex align-items-center" v-if="todo.completed===true" :key="todo.id">
-          <div class="form-check">
-            <label :class="{ terminer: todo.completed }">
-              <input type="checkbox" v-model="todo.completed" @click="replaceTodo">
-              {{ todo.name }}
-            </label>
-          </div>
-          <button @click="deleteTask" class="btn btn-outline-primary ms-auto">supprimer</button>
-        </li>
-      </ul>
-
+      <div class="form-check mt-2">
+        <label class="form-check-label" for="flexCheckDefault"><input class="form-check-input" type="checkbox" v-model="hideCompleted">
+          Masquer les tâches terminer
+        </label>
+      </div>
     </section>
   </main>
 </template>
@@ -67,7 +55,7 @@ import { ref } from 'vue'
 
 // tableau des tache a faire
 const todos = ref([
- /* {
+  {
     name: "une tache",
     completed: true,
     id: 1
@@ -91,11 +79,14 @@ const todos = ref([
     name: "une 5e tache",
     completed: false,
     id: 5
-  }*/
+  }
 ]);
 
 // la nouvelle tache
 const task = ref('');
+
+// masque les tâches terminer
+const hideCompleted = ref(false)
 
 // ajoute une tache au tableau todos
 const addTask = () => {
@@ -109,12 +100,19 @@ const addTask = () => {
 }
 
 const sortedTodo = () => {
+  const sortedTodo = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+
+  if (hideCompleted.value === true) {
+    return sortedTodo.filter(t => t.completed === false)
+  }
+  console.log(hideCompleted);
   
+  return sortedTodo
 }
 
 // supprime un tache
 const deleteTask = (e) => {
-  e.target.parentElement.remove()
+  return todos.value.splice(e.target.parentElement, 1)
 }
 
 </script>
